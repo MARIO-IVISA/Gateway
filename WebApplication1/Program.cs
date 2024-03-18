0c42788cf585520c5dbab5048bc98dbc2e8ebf9a
MARIO\mario <marioduarte.ivisa@gmail.com>
2024-03-17 22:09:56 -0300

    first commit

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,56 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("https://example.com") // Permitir apenas requisições deste domínio
+               .AllowAnyMethod() // Permitir qualquer método (GET, POST, etc.)
+               .AllowAnyHeader(); // Permitir qualquer cabeçalho
+    });
+});
+
+
+builder.Host.ConfigureAppConfiguration((context, config) =>
+{
+    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+});
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(
+                    s => s.AddPolicy("DefaultPolicy", builder =>
+                    {
+                        builder.AllowAnyOrigin() //qualquer dominio
+                               .AllowAnyMethod() //qualquer método (POST, PUT, DELETE, GET...)
+                               .AllowAnyHeader(); //qualquer parametro de cabeçalho
+                    }
+                        )
+                );
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+app.UseRouting();
+
+app.UseCors("DefaultPolicy");
+
+app.UseOcelot().Wait();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
